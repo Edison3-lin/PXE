15186dc6067e66fc4e7a2c1ab931b021894b0a53
edison3lin <edison.lin@quantatw.com>
2023-11-30 15:10:57 +0800

    Put TestManager.exe into windows startup

--- test.bat
+++ test.bat
@@ -2,5 +2,6 @@
 powershell set-executionpolicy Bypass -scope CurrentUser
 powershell set-executionpolicy Bypass -scope process
 @REM powershell start-process c:\TestManager\TestManager.exe -WindowStyle Hidden -verb RunAs
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v "TestManager.exe" /t REG_SZ /d "C:\TestManager\TestManager.exe" /f
 powershell start-process .\TestManager.exe -verb RunAs
 exit
