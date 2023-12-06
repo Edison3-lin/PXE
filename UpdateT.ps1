959c256d609cdec32d7397ae038a673d9b796966
edison3lin <edison.lin@quantatw.com>
2023-12-06 09:58:03 +0800

    Consider Windows service-related behaviors - 2

--- UpdateT.ps1
+++ UpdateT.ps1
@@ -3,16 +3,14 @@ function Down_Common()
     $ftpDirectory = "/TestManager/"
     $commonFilePath = ".\"
 
-    # 获取目录下的所有文件
-    $files = Get-ChildItem -Path $commonFilePath -File
-
-    # 删除所有文件
-    foreach ($file in $files) {
-        if($file.Name -ne "UpdateT.ps1")
-        {
-            Remove-Item $file.FullName -Force
-        }    
-    }
+    # # 删除c:\TestManager目錄下所有file
+    # $files = Get-ChildItem -Path $commonFilePath -File
+    # foreach ($file in $files) {
+    #     if($file.Name -ne "UpdateT.ps1")
+    #     {
+    #         Remove-Item $file.FullName -Force
+    #     }    
+    # }
 
     $ftpRequest = [System.Net.FtpWebRequest]::Create("$ftpServer$ftpDirectory")
     $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($username, $password)
@@ -26,6 +24,12 @@ function Down_Common()
     $dir = $directoryListing -split "`r`n"
     if( [int]$dir[-2] -gt [int]$version)
     {
+
+#(EdisonLin-20231206-)>>
+Start-Process -FilePath "C:\TestManager\Service_out.bat" -NoNewWindow -Wait
+#(EdisonLin-20231206-)<<
+
+
         $ftpDirectory = "/TestManager/$($dir[-2])/"
 
         $ftpRequest = [System.Net.FtpWebRequest]::Create("$ftpServer$ftpDirectory")
@@ -48,7 +52,11 @@ function Down_Common()
                 Write-Host "!!!<$fileName>: $($_.Exception.Message)"
             }
         }
-        Start-Process -FilePath ".\Test.bat" -Wait
+        # Start-Process -FilePath ".\Test.bat" -Wait
+#(EdisonLin-20231206-)>>
+Start-Process -FilePath "C:\TestManager\Service_in.bat" -NoNewWindow -Wait
+#(EdisonLin-20231206-)<<
+
     }    
     $ftpReader.Close()
     $ftpStream.Close()
