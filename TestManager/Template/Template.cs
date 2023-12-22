6832b6aac590830dd0d8777213cae59fd49c4cf2
edison3lin <edison.lin@quantatw.com>
2023-12-22 15:39:46 +0800

    1221-1

---
+++ TestManager/Template/Template.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Xml.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using CaptainWin.CommonAPI;
+
+namespace Template {
+    public class MyTemplate {
+        private const string TR = "C:\\TestManager\\TR_Result.json";
+        public static void Setup() {
+        }
+
+        public static void Run()
+        {
+            string ftpJson = System.IO.File.ReadAllText(TR);
+            JObject fjson = JObject.Parse(ftpJson);
+            int index = (int)fjson["Reboot"];
+
+            int DllIndex = 0;
+            //********* SIT 依序填寫執行的DLL的項目 /Start/
+            // DllIndex++; // 1
+            // if( DllIndex > index ) {
+            //     RecordDllIndex(DllIndex);
+            //     CaptainWin.CommonAPI.DoSleep.Sleep(3, 1);
+            // }    
+
+            DllIndex++; // 2
+            if( DllIndex > index ) {
+                RecordDllIndex(DllIndex);
+                CaptainWin.CommonAPI.SysInfo.GetWMI("Win32_BIOS");
+            }    
+
+            // DllIndex++; // 3
+            // if( DllIndex > index ) {
+            //     RecordDllIndex(DllIndex);
+            //     CaptainWin.CommonAPI.DoReboot.Reboot(10);
+            // }    
+
+            DllIndex++; // 4
+            if( DllIndex > index ) {
+                RecordDllIndex(DllIndex);
+                CaptainWin.CommonAPI.SysInfo.GetWMI("Win32_SMBIOSMemory");
+            }    
+
+            DllIndex++; // 4
+            if( DllIndex > index ) {
+                RecordDllIndex(DllIndex);
+                CaptainWin.CommonAPI.SysInfo.GetWMI("Win32_PhysicalMemoryLocation");
+            }    
+
+            DllIndex++; // 5
+            if( DllIndex > index ) {
+                RecordDllIndex(DllIndex);
+                CaptainWin.CommonAPI.SysInfo.GetWMI("Win32_MemoryArray");
+            }    
+
+            DllIndex++; // 6
+            if( DllIndex > index ) {
+                RecordDllIndex(DllIndex);
+                CaptainWin.CommonAPI.SysInfo.GetWMI("Win32_MemoryDevice");
+            }    
+
+            DllIndex++; // 7
+            if( DllIndex > index ) {
+                RecordDllIndex(DllIndex);
+                CaptainWin.CommonAPI.Culture.GetCulture();
+            }    
+
+            DllIndex++; // 4
+            if( DllIndex > index ) {
+                RecordDllIndex(DllIndex);
+                CaptainWin.CommonAPI.GetSystemInfo.GetOSVersion();
+                CaptainWin.CommonAPI.GetSystemInfo.GetSystemType();
+                CaptainWin.CommonAPI.GetSystemInfo.GetProcessorName();
+                CaptainWin.CommonAPI.GetSystemInfo.GetRunningTime();
+                CaptainWin.CommonAPI.GetSystemInfo.GetPhysicalMemory();
+                CaptainWin.CommonAPI.GetSystemInfo.GetAvailableMemory();
+                CaptainWin.CommonAPI.GetSystemInfo.GetCpuId();
+                CaptainWin.CommonAPI.GetSystemInfo.GetCPUCount();
+                CaptainWin.CommonAPI.GetSystemInfo.GetDiskDevice();
+            }    
+            //********* SIT 依序填寫執行的DLL的項目 /End/
+
+            RecordDllIndex(0);
+        }
+
+        public static void UpdateResults() {
+        }
+
+        public static void TearDown() {
+        }
+
+        public static void RecordDllIndex(int DllIndex) {
+            try {
+                string ftpJson = System.IO.File.ReadAllText(TR);
+                JObject fjson = JObject.Parse(ftpJson);
+                fjson["Reboot"] = DllIndex;
+                string updatedJson = fjson.ToString();
+                System.IO.File.WriteAllText(TR, updatedJson);
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"sdfsadfsdfsd An error occurred: {ex.Message}");
+            }
+        }   //RecordDllIndex    
+
+    } //Class1
+} //namespace
